9233ee3dfe881052926e57c83ffead46f4819bec
a-mrv <aydoganmrv@gmail.com>
2024-05-26 19:22:08 +0300

    Update

---
+++ ExamResultAPI/ExamResult.Domain/ExamResult.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamResultAPI.Domain
+{
+    public class ExamResult
+    {
+
+        public double MidtermGrade { get; set; }
+        public double FinalGrade { get; set; }
+        public double OverallResult { get; set; }
+        public double Quiz { get; set; }
+        public double Quiz1 { get; set; }
+        public double Quiz2 { get; set; }
+        public double Quiz3 { get; set; }
+
+    }
+}
---
+++ ExamResultAPI/ExamResultAPI/Controllers/ExamResultController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ExamResultAPI.Domain;
+using System;
+
+namespace ExamResultAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ExamResultController : ControllerBase
+    {
+        [HttpGet("quiz")]
+
+        public ExamResult AddQuiz(double quiz1, double quiz2, double quiz3)
+        {
+            double quiz = (quiz1 + quiz2 + quiz3) / 3;
+
+
+
+
+            var result = new ExamResult
+            {
+                Quiz = quiz,
+            };   
+
+            return result;
+
+        }
+
+        [HttpGet("action")]
+
+        public ExamResult Add(double quiz, double midtermGrade, double finalGrade)
+        {
+          
+
+            double overallGrade =  (quiz*0.1+ midtermGrade*0.3+ finalGrade*0.6);
+
+
+            var result = new ExamResult
+            {   Quiz=  quiz,
+                MidtermGrade = midtermGrade,
+                FinalGrade = finalGrade,
+                OverallResult = overallGrade
+            };
+
+            return result; 
+
+        }
+    }
+}
